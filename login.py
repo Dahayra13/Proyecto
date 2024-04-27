e5aae978808d0ae7fd8e770c8d733020e03942eb
Xiomara <119473082+Dahayra13@users.noreply.github.com>
2024-04-27 07:17:38 -0500

    Update login.py

--- login.py
+++ login.py
@@ -48,6 +48,10 @@ if __name__ == '__main__':
     </div>
     """, unsafe_allow_html=True)
 
-   
+# Si las credenciales son correctas, mostrar un mensaje de éxito
+            st.markdown('<div id="result-message" style="color: green; text-align: center; margin-top: 1rem;">Inicio de sesión exitoso.</div>', unsafe_allow_html=True)
+        else:
+            # Si las credenciales son incorrectas, mostrar un mensaje de error
+            st.markdown('<div id="result-message" style="color: red; text-align: center; margin-top: 1rem;">Credenciales incorrectas. Por favor, inténtalo de nuevo.</div>', unsafe_allow_html=True)
 
    
