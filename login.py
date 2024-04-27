2d4f16e127561480f1fe0e485656a4f4242c1789
Xiomara <119473082+Dahayra13@users.noreply.github.com>
2024-04-27 07:35:02 -0500

    Update login.py

--- login.py
+++ login.py
@@ -20,6 +20,15 @@ if os.path.exists(logo_path):
 else:
     st.error("No se encontró el archivo de logo de la universidad.")
 
+# Diseño mejorado del formulario de inicio de sesión
+st.markdown("""
+<div style="background-color: #d6eb9d; padding: 2rem; border-radius: 0.7rem; box-shadow: 0 1024px 768x rgba(0, 0, 0, 0.1); max-width: 600px; margin: auto;">
+    <div style="text-align: center; margin-bottom: 2rem;">
+        <h1 style="color: #ad1c2d;">𝐁𝐢𝐞𝐧𝐯𝐞𝐧𝐢𝐝𝐨 𝐚𝐥 𝐏𝐨𝐫𝐭𝐚𝐥 𝐝𝐞 𝐌𝐚𝐭𝐫𝐢𝐜𝐮𝐥𝐚</h1>
+        <p style="color: #0d0c0c;">Aᴄᴄᴇsᴏ ᴀᴜᴛᴏʀɪᴢᴀᴅᴏ ᴀʟ ᴄᴜʀʀɪ́ᴄᴜʟᴏ ᴀᴄᴀᴅᴇ́ᴍɪᴄᴏ</p>
+    </div>
+    <form>
+
 # Ejecutar la aplicación
 if __name__ == '__main__':
     # Obtener las credenciales del usuario
