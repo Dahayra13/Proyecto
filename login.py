2af011d8381490e98676943d59ce1deada0da056
Xiomara <119473082+Dahayra13@users.noreply.github.com>
2024-04-27 07:08:04 -0500

    Update login.py

--- login.py
+++ login.py
@@ -13,7 +13,6 @@ import os
 usuario_correcto = "41650931"
 contrasenia_correcta = "cayetano"
 
-
 # Cargar logo de la universidad
 logo_path =  "Logo_upch.png"
 if os.path.exists  (logo_path):
@@ -21,37 +20,48 @@ if os.path.exists  (logo_path):
 else:
     st.error("No se encontró el archivo de logo de la universidad.")
 
-# Diseño mejorado del formulario de inicio de sesión
-st.markdown("""
-<div style="background-color: #d6eb9d; padding: 2rem; border-radius: 0.7rem; box-shadow: 0 1024px 768x rgba(0, 0, 0, 0.1); max-width: 600px; margin: auto;">
-    <div style="text-align: center; margin-bottom: 2rem;">
-        <h1 style="color: #ad1c2d;">𝐁𝐢𝐞𝐧𝐯𝐞𝐧𝐢𝐝𝐨 𝐚𝐥 𝐏𝐨𝐫𝐭𝐚𝐥 𝐝𝐞 𝐌𝐚𝐭𝐫𝐢𝐜𝐮𝐥𝐚</h1>
-        <p style="color: #0d0c0c;">Aᴄᴄᴇsᴏ ᴀᴜᴛᴏʀɪᴢᴀᴅᴏ ᴀʟ ᴄᴜʀʀɪ́ᴄᴜʟᴏ ᴀᴄᴀᴅᴇ́ᴍɪᴄᴏ</p>
-    </div>
-    <form>
-        <div class="form-group">
-            <label for="usuario" style="color: #030303;">𝚄𝚂𝚄𝙰𝚁𝙸𝙾:</label>
-            <input type="text" class="form-control" id="" placeholder="Ingrese su usuario" style="width: 100%; padding: 0.7rem; margin-bottom: 1rem;" required>
-        </div>
-        <div class="form-group">
-            <label for="contrasenia" style="color: #030303;">𝙲𝙾𝙽𝚃𝚁𝙰𝚂𝙴𝙽̃𝙰:</label>
-            <input type="password" class="form-control" id="𝙲𝙾𝙽𝚃𝚁𝙰𝚂𝙴𝙽̃A" placeholder="Ingrese su contraseña" style="width: 100%; padding: 0.5rem; margin-bottom: 1rem;" required>
-        </div>
-        <div class="form-group" style="text-align: center;">
-            <button type="button" class="btn btn-primary" onclick="handleLogin()" style="padding: 0.5rem 2rem; background-color: #9c2121; color: grey; border: none; border-radius: 0.3rem;">𝐈𝐧𝐢𝐜𝐢𝐚𝐫 𝐬𝐞𝐬𝐢𝐨́𝐧</button>
-        </div>
-    </form>
-</div>
-""", unsafe_allow_html=True)
+# Función para autenticar al usuario
+def authenticate(username_entered, password_entered):
+    if username_entered == usuario_correcto and password_entered == contrasenia_correcta:
+        return True
+    else:
+        return False
 
 # Ejecutar la aplicación
 if __name__ == '__main__':
+    st.markdown("""
+    <div style="background-color: #d6eb9d; padding: 2rem; border-radius: 0.7rem; box-shadow: 0 1024px 768x rgba(0, 0, 0, 0.1); max-width: 600px; margin: auto;">
+        <div style="text-align: center; margin-bottom: 2rem;">
+            <h1 style="color: #ad1c2d;">𝐁𝐢𝐞𝐧𝐯𝐞𝐧𝐢𝐝𝐨 𝐚𝐥 𝐏𝐨𝐫𝐭𝐚𝐥 𝐝𝐞 𝐌𝐚𝐭𝐫𝐢𝐜𝐮𝐥𝐚</h1>
+            <p style="color: #0d0c0c;">Aᴄᴄᴇsᴏ ᴀᴜᴛᴏʀɪᴢᴀᴅᴏ ᴀʟ ᴄᴜʀʀɪ́ᴄᴜʟᴏ ᴀᴄᴀᴅᴇ́ᴍɪᴄᴏ</p>
+        </div>
+        <form>
+            <div class="form-group">
+                <label for="usuario" style="color: #030303;">𝚄𝚂𝚄𝙰𝚁𝙸𝙾:</label>
+                <input type="text" class="form-control" id="usuario" placeholder="Ingrese su usuario" style="width: 100%; padding: 0.7rem; margin-bottom: 1rem;" required>
+            </div>
+            <div class="form-group">
+                <label for="contrasenia" style="color: #030303;">𝙲𝙾𝙽𝚃𝚁𝙰𝚂𝙴𝙽̃𝙰:</label>
+                <input type="password" class="form-control" id="contrasenia" placeholder="Ingrese su contraseña" style="width: 100%; padding: 0.5rem; margin-bottom: 1rem;" required>
+            </div>
+            <div class="form-group" style="text-align: center;">
+                <button type="button" class="btn btn-primary" onclick="handleLogin()" style="padding: 0.5rem 2rem; background-color: #9c2121; color: grey; border: none; border-radius: 0.3rem;">𝐈𝐧𝐢𝐜𝐢𝐚𝐫 𝐬𝐞𝐬𝐢𝐨́𝐧</button>
+            </div>
+        </form>
+        <div id="result-message" style="color: red; text-align: center; margin-top: 1rem;"></div>
+    </div>
+    """, unsafe_allow_html=True)
+
     username_entered = st.text_input("Usuario", "")
     password_entered = st.text_input("Contraseña", "", type="password")
+
     if st.button("Iniciar sesión"):
         if authenticate(username_entered, password_entered):
-            st.success("¡Inicio de sesión exitoso!")
+            st.markdown('<div id="result-message" style="color: green; text-align: center; margin-top: 1rem;">Inicio de sesión exitoso.</div>', unsafe_allow_html=True)
         else:
-            st.write("Credenciales incorrectas. Por favor, inténtalo de nuevo.")
+            st.markdown('<div id="result-message" style="color: red; text-align: center; margin-top: 1rem;">Credenciales incorrectas. Por favor, inténtalo de nuevo.</div>', unsafe_allow_html=True)
+
+
+
 
    
