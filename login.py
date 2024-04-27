3ad6e6f0cc1415d2af8abab3678489277e8e34b6
Xiomara <119473082+Dahayra13@users.noreply.github.com>
2024-04-27 13:59:10 -0500

    Update login.py

--- login.py
+++ login.py
@@ -1,6 +1,5 @@
 import streamlit as st
 from PIL import Image
-import time
 
 # Configurar el diseño de la página
 st.set_page_config(layout="centered")
@@ -12,61 +11,27 @@ university_logo = Image.open("Logo_upch.png")
 User = "41650931"
 Password = "cayetano"
 
+# Crear el diseño del formulario de inicio de sesión
 st.markdown(
     """
     <style>
     .stApp {
-        background-color: white;
+        background-color: #070808;
         display: flex;
         justify-content: center;
         align-items: center;
         height: 100vh;
     }
     .login-container {
-        background-color: white;
-        width: 400px;
-        padding: 40px;
-        border-radius: 30px;
-        box-shadow: 0 0 20px rgba(0, 0, 0, 0.2);
+        background-color: #9ee6cf;
+        width: 300px;
+        padding: 30px;
+        border-radius: 20px;
+        align-items: center;
+        box-shadow: 0 0 10px rgba(0, 0, 0, 0.1);
     }
     .portal-title {
         text-align: center;
-        color: #1A4D80;
-        margin-bottom: 20px;
-        font-size: 32px;
-        font-weight: bold;
-    }
-    .form-field {
-        margin-bottom: 20px;
-    }
-    .form-field label {
-        font-size: 16px;
-        font-weight: bold;
-        color: #1A4D80;
-        margin-bottom: 5px;
-    }
-    .form-field input {
-        width: 100%;
-        padding: 10px;
-        border: none;
-        border-radius: 10px;
-        background-color: #f0f0f0;
-        font-size: 16px;
-    }
-    .submit-button {
-        width: 100%;
-        padding: 10px;
-        border: none;
-        border-radius: 10px;
-        background-color: #1A4D80;
-        color: white;
-        font-size: 16px;
-        font-weight: bold;
-        cursor: pointer;
-        transition: background-color 0.3s ease;
-    }
-    .submit-button:hover {
-        background-color: #134266;
     }
     </style>
     """,
@@ -74,13 +39,13 @@ st.markdown(
 )
 
 st.image(university_logo, use_column_width=True)
-st.markdown("<div class='login-container'><h1 class='portal-title'>🎓 Portal de Matrícula - UPCH</h1>", unsafe_allow_html=True)
+st.markdown("<div class='login-container'><h3 class='portal-title'>𝐏𝐨𝐫𝐭𝐚𝐥 𝐝𝐞 𝐩𝐥𝐚𝐧 𝐝𝐞 𝐄𝐬𝐭𝐮𝐝𝐢𝐨𝐬</h3>", unsafe_allow_html=True)
 
 # Crear los campos de usuario y contraseña
 with st.form(key="login_form"):
-    username = st.text_input(" Usuario: ", value=User)
-    password = st.text_input("Contraseña:", type="password", value=Password)
-    submit = st.form_submit_button("Iniciar Sesión", use_container_width=True)
+    username = st.text_input(" 𝚄𝚜𝚎𝚛: ", value=User)
+    password = st.text_input("𝙿𝚊𝚜𝚜𝚠𝚘𝚛𝚍:", type="password", value=Password)
+    submit = st.form_submit_button("𝕃𝕠𝕘 𝕀𝕟")
 
 # Cerrar el div del login-container
 st.markdown("</div>", unsafe_allow_html=True)
@@ -88,9 +53,7 @@ st.markdown("</div>", unsafe_allow_html=True)
 # Verificar las credenciales y mostrar un mensaje de éxito o error
 if submit:
     if username == User and password == Password:
-        st.success("¡Correct user!")
-        time.sleep(2)
-        st.balloons()
+        st.success("¡correct user!")
     else:
         st.error("Invalid username or password. Please try again.")
 
