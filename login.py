4f31c8f7f52767a4eb09e34d55ff1fc506537900
Xiomara <119473082+Dahayra13@users.noreply.github.com>
2024-04-27 07:13:46 -0500

    Update login.py

--- login.py
+++ login.py
@@ -9,9 +9,13 @@ Original file is located at
 import streamlit as st
 import os
 
-# Definir credenciales de inicio de sesión
-usuario_correcto = "41650931"
-contrasenia_correcta = "cayetano"
+# Función para autenticar al usuario
+def authenticate(username_entered, password_entered):
+    if username_entered == usuario_correcto and password_entered == contrasenia_correcta:
+        return True
+    else:
+        return False
+
 
 # Cargar logo de la universidad
 logo_path =  "Logo_upch.png"
@@ -19,13 +23,6 @@ if os.path.exists  (logo_path):
     logo = st.image (logo_path, width = 300, use_column_width = False)
 else:
     st.error("No se encontró el archivo de logo de la universidad.")
-
-# Función para autenticar al usuario
-def authenticate(username_entered, password_entered):
-    if username_entered == usuario_correcto and password_entered == contrasenia_correcta:
-        return True
-    else:
-        return False
 
 # Ejecutar la aplicación
 if __name__ == '__main__':
