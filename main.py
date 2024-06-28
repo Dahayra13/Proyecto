1bd1e72c9b7c7ce0f0cd307bd11c12dbf1afc3da
Xiomara <119473082+Dahayra13@users.noreply.github.com>
2024-06-28 13:27:50 -0500

    Update main.py

--- main.py
+++ main.py
@@ -9,16 +9,9 @@ from requerimiento_ambientes import app as requerimiento_ambientes_app
 from asignacion_alumnos import app as asignacion_alumnos_app
 from optimizar_horarios import app as optimizar_horarios_app
 import os
-import base64
 
 def main():
     st.set_page_config(layout="wide", initial_sidebar_state='collapsed', page_title="Gestión de Cursos UPCH")
-
-    current_dir = os.path.dirname(os.path.abspath(_file_))
-    logo_path = os.path.join(current_dir, "logo_upch.png")
-
-    with open(logo_path, "rb") as image_file:
-        encoded_logo = base64.b64encode(image_file.read()).decode()
 
     User = "0000"  # Tu usuario
     Password = "0000"  # Tu contraseña
@@ -29,7 +22,7 @@ def main():
     set_background()
 
     if not st.session_state.logged_in:
-        login(encoded_logo, User, Password)
+        login(User, Password)
     else:
         app = MultiApp()
 
@@ -48,5 +41,6 @@ def main():
                 app_page['function']()
                 break
 
-if _name_ == "_main_":
+if __name__ == "__main__":
     main()
+
