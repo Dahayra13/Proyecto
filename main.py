a0b4b640e2e26c72da301c17523d9a2d7ed4debb
Xiomara <119473082+Dahayra13@users.noreply.github.com>
2024-06-28 13:13:27 -0500

    Update main.py

--- main.py
+++ main.py
@@ -11,29 +11,21 @@ from optimizar_horarios import app as optimizar_horarios_app
 import os
 
 def main():
-    # Configurar la página de Streamlit
     st.set_page_config(layout="wide", initial_sidebar_state='collapsed', page_title="Gestión de Cursos UPCH")
 
-    # Definir las credenciales de usuario y contraseña
     User = "0000"  # Tu usuario
     Password = "0000"  # Tu contraseña
 
-    # Inicializar el estado de sesión de inicio de sesión
     if "logged_in" not in st.session_state:
         st.session_state.logged_in = False
 
-    # Establecer el fondo de la aplicación
     set_background()
 
-    # Verificar si el usuario ha iniciado sesión
     if not st.session_state.logged_in:
-        # Si no ha iniciado sesión, llamar a la función de inicio de sesión
         login(User, Password)
     else:
-        # Si ya ha iniciado sesión, mostrar la aplicación
         app = MultiApp()
 
-        # Agregar las diferentes secciones de la aplicación
         app.add_app("Home", home_app)
         app.add_app("Modelar Salones", modelar_salones_app)
         app.add_app("Modelar Ambientes", modelar_ambientes_app)
@@ -42,16 +34,14 @@ def main():
         app.add_app("Asignación de Alumnos", asignacion_alumnos_app)
         app.add_app("Optimización de Horarios", optimizar_horarios_app)
 
-        # Mostrar el menú desplegable en la barra lateral para seleccionar la sección
         selected_app = st.sidebar.selectbox("Selecciona una sección", [app['title'] for app in app.apps])
 
-        # Ejecutar la sección seleccionada
         for app_page in app.apps:
             if app_page['title'] == selected_app:
                 app_page['function']()
                 break
 
-if __name__ == "__main__":
+if _name_ == "_main_":
     main()
 
 
