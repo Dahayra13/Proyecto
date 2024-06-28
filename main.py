a07209f7e1a5ba0dfd839c5247fa5b07610cf57c
Xiomara <119473082+Dahayra13@users.noreply.github.com>
2024-06-28 17:31:44 -0500

    Update main.py

--- main.py
+++ main.py
@@ -12,33 +12,27 @@ import os
 import base64
 
 def main():
-    # Establece la configuración de la página
     st.set_page_config(layout="wide", initial_sidebar_state='collapsed', page_title="Gestión de Cursos UPCH")
 
-    # Carga el logotipo de la UPCH
-    current_dir = os.path.dirname(os.path.abspath(__file__))
+    current_dir = os.path.dirname(os.path.abspath(_file_))
     logo_path = os.path.join(current_dir, "logo_upch.png")
+
     with open(logo_path, "rb") as image_file:
         encoded_logo = base64.b64encode(image_file.read()).decode()
 
-    # Credenciales de inicio de sesión
-    User = "0000"
-    Password = "0000"
+    User = "0000"  # Tu usuario
+    Password = "0000"  # Tu contraseña
 
-    # Verifica si el usuario ha iniciado sesión
     if "logged_in" not in st.session_state:
         st.session_state.logged_in = False
 
     set_background()
 
-    # Si el usuario no ha iniciado sesión, muestra la pantalla de inicio de sesión
     if not st.session_state.logged_in:
         login(encoded_logo, User, Password)
     else:
-        # Crea una aplicación MultiApp
         app = MultiApp()
 
-        # Agrega las secciones de la aplicación
         app.add_app("Home", home_app)
         app.add_app("Modelar Salones", modelar_salones_app)
         app.add_app("Modelar Ambientes", modelar_ambientes_app)
@@ -47,42 +41,12 @@ def main():
         app.add_app("Asignación de Alumnos", asignacion_alumnos_app)
         app.add_app("Optimización de Horarios", optimizar_horarios_app)
 
-        # Permite al usuario seleccionar la sección que desea ver
         selected_app = st.sidebar.selectbox("Selecciona una sección", [app['title'] for app in app.apps])
 
-        # Establece el estilo de la fuente y el color de fondo
-        st.markdown("""
-        <style>
-        body {
-            font-family: 'Times New Roman', serif;
-            color: #333;
-        }
-        .cycle-1 {
-            background-color: #f0f8ff; /* Azul pastel */
-        }
-        .cycle-2 {
-            background-color: #f5f5dc; /* Beige pastel */
-        }
-        .cycle-3 {
-            background-color: #e6e6fa; /* Lavanda pastel */
-        }
-        .cycle-4 {
-            background-color: #faf0e6; /* Salmón pastel */
-        }
-        .cycle-5 {
-            background-color: #f0ffff; /* Celeste pastel */
-        }
-        </style>
-        """, unsafe_allow_html=True)
-
-        # Muestra la sección seleccionada
         for app_page in app.apps:
             if app_page['title'] == selected_app:
                 app_page['function']()
                 break
 
-if __name__ == "__main__":
+if _name_ == "_main_":
     main()
-
-
-
